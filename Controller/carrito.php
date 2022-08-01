79f73d5693e6320d630373ec429d2fca11fd8fd9
FelipeRey97 <andresfrey97@gmail.com>
2022-08-01 11:47:06 -0500

    Corrección Patron MVC

--- Controller/carrito.php
+++ Controller/carrito.php
@@ -1,54 +1,26 @@
 <?php   
 session_start();
 
+require_once('../Model/M_carrito.php');
 
 $sesionId= $_REQUEST['sesionId'];
 $artId = $_REQUEST['artId'];
 $Cant = $_REQUEST['Cant'];
 
-$conexion1 = mysqli_connect("localhost","root","","proyecto") 
-or die ("problemas con la conexion"); 
+$car1 = new Carrito ();
 
-$datos = mysqli_query($conexion1,"SELECT * FROM carrito 
-    JOIN articulo 
-    ON articuloId = artId
-    WHERE articuloId = $artId AND sesionId = '$sesionId'") or die("problemas en el select" . mysqli_error($conexion1));
-if(isset($datos)){
+if(isset($_REQUEST['delete'])){
 
-    while($dat = mysqli_fetch_array($datos)){
-
-        $idArt = $dat['articuloId'];
-        $idSesion = $dat ['sesionId'];
-        $cantidad = $dat['artCarroCant'];
-        $carId = $dat['carId'];
-    }
-  
+    $car1->borrarCarrito($artId,$sesionId);
+    header("Location:" .$_SERVER['HTTP_REFERER']."");
 }
 
+if(isset($_REQUEST['Cant'])){
 
-if(isset($idArt) && $idArt == $artId && $idSesion == $sesionId){
-
-    $Cant = $cantidad + 1;
-
-    echo mysqli_query($conexion1, "UPDATE carrito SET artCarroCant = $Cant   
-    WHERE articuloId = $idArt AND sesionId = '$sesionId' AND carId = $carId ");
-   ?><script>
-    r=1;
-   </script>
- <?php   
-}else if($idArt == ""){
-
-    echo mysqli_query($conexion1,"INSERT INTO carrito (sesionId,articuloId,artCarroCant) 
-    VALUES ('$sesionId',$artId,$Cant)") or die ("problemas en el insert" . mysqli_error($conexion1));
-
-?><script>
-r=1;
-</script>
-<?php 
-
-}
+    $car1->AñadirCarrito($sesionId,$artId,$Cant);
 
 header("location:" .$_SERVER['HTTP_REFERER']. "");
     
+}
 
 ?>
--- Model/M_carrito.php
+++ Model/M_carrito.php
@@ -1,7 +1,65 @@
 <?php  
 
 
+    class Carrito {
 
+
+        private $car;
+
+        public function __construct(){
+
+
+            require_once('../Model/ConexionBD.php');
+            $this->car = $conexionBD;
+
+
+        }
+        public function AñadirCarrito($sesionId,$artId,$Cant){
+
+
+            $datos = $this->car->query("SELECT * FROM carrito 
+            JOIN articulo 
+            ON articuloId = artId
+            WHERE articuloId = $artId AND sesionId = '$sesionId'");
+
+            if(isset($datos)){
+
+             while($dat = mysqli_fetch_array($datos)){
+
+             $idArt = $dat['articuloId'];
+             $idSesion = $dat ['sesionId'];
+             $cantidad = $dat['artCarroCant'];
+             $carId = $dat['carId'];
+            
+                 }
+  
+            }
+
+            if(isset($idArt) && $idArt == $artId && $idSesion == $sesionId){
+
+                $Cant = $cantidad + 1;
+            
+                $this->car->query("UPDATE carrito SET artCarroCant = $Cant   
+                WHERE articuloId = $idArt AND sesionId = '$sesionId' AND carId = $carId ");
+                
+            }else if($idArt == ""){
+            
+                $this->car->query("INSERT INTO carrito (sesionId,articuloId,artCarroCant) 
+                VALUES ('$sesionId',$artId,$Cant)") or die ("problemas en el insert" . mysqli_error($conexion1));
+            
+            }
+
+        }
+
+        public function borrarCarrito($artId,$sesionId){
+
+            $this->car->query("DELETE FROM carrito 
+            WHERE articuloId = $artId AND sesionId = '$sesionId' ");
+
+        }
+
+
+    }
 
 
 
